f43ab1dbc35014529d491b15cc35dcd9144c2b27
Bharat Siddh <bharatsiddh295@gmail.com>
2023-10-26 11:13:47 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('body'),
+    smooth: true
+});
+
+
+gsap.from(".text",{
+    y:100,
+    duration:2,
+    delay:.3,
+    
+
+})
+
+
+
+
+
+
